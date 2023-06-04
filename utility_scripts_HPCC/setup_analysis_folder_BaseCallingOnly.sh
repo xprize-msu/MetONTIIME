e14d1b8b9379f9b3a736ab65cea538135d14822e
Nicholas Louis Panchy <panchyni@msu.edu>
2023-06-04 13:45:23 -0400

    Updated setup utility scritps with a new config that uses all barcodes so we can look at off target assignments/validated the field records.

--- utility_scripts_HPCC/setup_analysis_folder_BaseCallingOnly.sh
+++ utility_scripts_HPCC/setup_analysis_folder_BaseCallingOnly.sh
@@ -1,5 +1,5 @@
 ln -s /mnt/research/xprize23/gitclones/MetONTIIME/Launch_MinION_mobile_lab_BaseCallingOnly.sh .
 ln -s /mnt/research/xprize23/gitclones/MetONTIIME/MinION_mobile_lab_noMet_NoMetONTIIME.R .
 ln -s /mnt/research/xprize23/gitclones/MetONTIIME/MetONTIIME.sh .
-cp config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_dbSILVA_id92.R .
-ln -s /mnt/research/xprize23/gitclones/MetONTIIME/config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_dbSILVA_id92.R config_MinION_mobile_lab.R
+cp -r /mnt/research/xprize23/gitclones/MetONTIIME/config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_AllBarcodes.R .
+ln -s config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_AllBarcodes.R config_MinION_mobile_lab.R
--- utility_scripts_HPCC/setup_analysis_folder_SLURM.sh
+++ utility_scripts_HPCC/setup_analysis_folder_SLURM.sh
@@ -1,6 +1,6 @@
 ln -s /mnt/research/xprize23/gitclones/MetONTIIME/Launch_MinION_mobile_lab_DemuxAnalysis.sh .
 ln -s /mnt/research/xprize23/gitclones/MetONTIIME/MinION_mobile_lab_noMet_NoMetONTIIME.R .
 ln -s /mnt/research/xprize23/gitclones/MetONTIIME/MetONTIIME.sh .
-cp /mnt/research/xprize23/gitclones/MetONTIIME/config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_dbSILVA_id92.R .
-ln -s config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_dbSILVA_id92.R config_MinION_mobile_lab.R
+cp /mnt/research/xprize23/gitclones/MetONTIIME/config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_AllBarcodes.R .
+ln -s config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_AllBarcodes.R config_MinION_mobile_lab.R
 python /mnt/research/xprize23/gitclones/MetONTIIME/utility_scripts_HPCC/parse_seq_metadata_SLURM.py $1
