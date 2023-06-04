8b31f813072470a39c0839801d1011259ae98ebc
Nicholas Louis Panchy <panchyni@msu.edu>
2023-06-04 09:49:02 -0400

    Update setup folders to soft link the local copy of the config file.

--- utility_scripts_HPCC/setup_analysis_folder.sh
+++ utility_scripts_HPCC/setup_analysis_folder.sh
@@ -2,5 +2,5 @@ ln -s /mnt/research/xprize23/gitclones/MetONTIIME/Launch_MinION_mobile_lab_Demux
 ln -s /mnt/research/xprize23/gitclones/MetONTIIME/MinION_mobile_lab_noMet_NoMetONTIIME.R .
 ln -s /mnt/research/xprize23/gitclones/MetONTIIME/MetONTIIME.sh .
 cp /mnt/research/xprize23/gitclones/MetONTIIME/config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_dbSILVA_id92.R .
-ln -s /mnt/research/xprize23/gitclones/MetONTIIME/config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_dbSILVA_id92.R config_MinION_mobile_lab.R
+ln -s config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_dbSILVA_id92.R config_MinION_mobile_lab.R
 python /mnt/research/xprize23/gitclones/MetONTIIME/utility_scripts_HPCC/parse_seq_metadata.py $1
---
+++ utility_scripts_HPCC/setup_analysis_folder_BaseCallingOnly.sh
@@ -0,0 +1,5 @@
+ln -s /mnt/research/xprize23/gitclones/MetONTIIME/Launch_MinION_mobile_lab_BaseCallingOnly.sh .
+ln -s /mnt/research/xprize23/gitclones/MetONTIIME/MinION_mobile_lab_noMet_NoMetONTIIME.R .
+ln -s /mnt/research/xprize23/gitclones/MetONTIIME/MetONTIIME.sh .
+cp config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_dbSILVA_id92.R .
+ln -s /mnt/research/xprize23/gitclones/MetONTIIME/config_MinION_mobile_lab_XPRIZE23_FLO_MIN114_SQK_NBD114_24_dbSILVA_id92.R config_MinION_mobile_lab.R
